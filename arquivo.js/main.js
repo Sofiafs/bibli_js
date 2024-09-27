115a7430f9139fd1495c5a9d88dbee46e0eebc8f
Davi Correia <davicorreiao07@gmail.com>
2024-09-27 16:56:53 -0300

    Sexto commit

--- arquivo.js/main.js
+++ arquivo.js/main.js
@@ -1,64 +1,56 @@
 class Livro{
     constructor(titulo){
         this.titulo = titulo
-        console.log("Novo livro adicionado")
+        console.log("Novo livro adicionado!")
     }
     
     exibirInformacoes(){
-        console.log('titulo: ${this.titulo}');
+        console.log(`titulo: ${this.titulo}`);
     }
 }
-
-class Biblioetca{
+ 
+class Biblioteca{
     constructor(){
-        this.livros = []
+        this.livros = [];
     }
     adicionarLivros(livro){
         this.livros.push(livro);
     }
     removerLivro(livro){
         this.livros = this.livros.filter(livro => livro.aluno != livro);
+        console.log(`livro com o titulo ${titulo} remvido`)
     }
     exibirLivros(){
-        this.livros.forEach(livro => livro.exibirInformacoes());
+        const lista = document.getElementById("listaLivro")
+        lista.innerHTML = "";
+        this.livros.forEach(livro => {
+            const li = document.createElement("li");
+            li.textContent = livro.exibirInformacoes();
+            lista.appendChild(li);
+            console.log(biblioteca.exibirLivros(livro));
+            console.log("Exibir livros!")
+        });
     }
 }
+let biblioteca = new Biblioteca
 
-// let biblioteca = new Biblioetca();
-// let livro1 = new Livro('O Senhor dos Anéis');
-// biblioteca.adicionarLivros(livro1);
-
-// let livro2 = new Livro('Diário de Anne Frank');
-// biblioteca.adicionarLivros(livro2);
-
-// console.log('Livros na biblioteca:');
-// biblioteca.exibirLivros();
-
-// biblioteca.removerLivro(1);
-
-// console.log('Livros após remoção: ');
-// biblioteca.exibirLivros();
-// document.write()
-
-document.addEventListener("DOMContentLoaded", () => {
-    const biblioteca
-})
 const btnAdd = document.querySelector('.btnAdd');
 btnAdd.addEventListener('click', () => {
 
     const titulo = document.getElementById('titulo').value;
 
     if (titulo) {
-        const novolivro = new Livro(titulo);
-        biblioetca.adicionarLivros(novolivro);
+        const livro = new Livro(titulo);
+        biblioteca.adicionarLivros(livro);
+        console.log('Cheguei no IF')
 
+    } else {
+        alert('Preencha o campo de título.');
     }
+    });
 
-console.log('conexão realizada!!!!');
-
-
-console.log(titulo);
-document.write(titulo)
-
-
-})
+const bntExi = document.querySelector('.bntExi');
+    bntExi.addEventListener('click', () => {
+    biblioteca.exibirLivros();
+    console.log('bntExi')
+});
